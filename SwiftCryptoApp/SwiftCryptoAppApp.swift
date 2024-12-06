e70ed4ddfe17aa697e0357074d2075f299ecf118
Akshay kumar shaw <akshay.8459361606@gmail.com>
2024-12-06 15:29:18 +0530

    Initial Commit

---
+++ SwiftCryptoApp/SwiftCryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoAppApp.swift
+//  SwiftCryptoApp
+//
+//  Created by Akshay kumar shaw on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
